dc30807d588b629d26ac19524c00b309c1f5292b
im-pingo <cczjp89@gmail.com>
2023-04-21 16:15:24 +0800

    add Executable

--- syservice.go
+++ syservice.go
@@ -77,6 +77,7 @@ func (s *syservice) InitCommand() ([]*cobra.Command, error) {
 				Description:      s.flags.Description,
 				WorkingDirectory: s.flags.WorkDir,
 				Arguments:        strings.Split(s.flags.Args, " "),
+				Executable:       gfile.SelfPath(),
 			})
 
 			if err != nil {
